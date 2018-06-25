8ebcd17f4cff1ee4f5f5237b311bf7470ea6ffaa
thamma513 <thamma513@gmail.com>
2018-06-25 17:50:36 -0600

    added a contacts page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 class Contact extends Component {
     state = {
@@ -6,9 +6,21 @@ class Contact extends Component {
     }
     render () {
         return (
-            <h2>This is where my contact info goes!!</h2>
+            <Fragment>
+            <h2>Check out my other projects!!</h2>
+            <p>My Portfolio
+
+            </p>
+            <p>
+            <a href={"https://github.com/thamma513"}>My Github</a>
+            </p>
+            <p>
+                <a href={"https://www.linkedin.com/in/tyler-hamilton-01a636143/"}>My LinkedIn</a>
+            </p>
+            </Fragment>
         );
     }
 }
+
 
 export default Contact;
