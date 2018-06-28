37a2ac4339c0c905f2b4f632be71229b1bfc39d2
thamma513 <thamma513@gmail.com>
2018-06-28 08:40:23 -0600

    pictures on page

--- src/components/pages/Memory.js
+++ src/components/pages/Memory.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
+import img from "../../img.json";
 
 class Memory extends Component {
     state = {
+        img: []
 
     }
+
+    
+  componentDidMount() {
+    this.setState({img});
+  }
+
     render() {
         return (
-            <h2>This is where stuff goes!!</h2>
+            <div>
+            <h4>Images for '{this.state.img.img}':</h4>
+            <ul className="list-group search-results">
+                    {this.state.img.map(img => <li className="list-group-item" key={img.img}><img src={img.img} alt="Img" className="img-thumbnail" /></li>)}
+            </ul>
+            </div>
         );
     }
 }
