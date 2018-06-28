de669837091890c3f8fd74348a90ab75ea9c5094
thamma513 <thamma513@gmail.com>
2018-06-28 13:03:42 -0600

    counter added

--- src/components/pages/Memory.js
+++ src/components/pages/Memory.js
@@ -1,9 +1,12 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import img from "../../img.json";
 
 class Memory extends Component {
     state = {
-        img: []
+        img: [],
+        clicked:[],
+        score: 0,
+        highscore: 0
 
     }
 
@@ -11,14 +14,25 @@ class Memory extends Component {
   componentDidMount() {
     this.setState({img});
   }
+ 
+  handleIncrement = () => {
+    // We always use the setState method to update a component's state
+    this.setState({ score: this.state.score + 1 });
+  };
 
     render() {
         return (
-            <div className="container text-center">
-                <div className="row sm-4">
-                    {this.state.img.map(img => <div key={img.img}><img src={img.img} alt="Img" className="img" /></div>)}
-                </div>
+        <Fragment>
+            <div className="jumbotron">
+            <h2>score: {this.state.score} </h2>
+            <h2>this is where the highscore goes:0</h2>
             </div>
+                <div className="container text-center">
+                    <div className="row sm-4">
+                        {this.state.img.map(img => <div onClick={this.handleIncrement} key={img.img}><img src={img.img} alt="Img" className="img" /></div>)}
+                    </div>
+                </div>
+        </Fragment>
         );
     }
 }
